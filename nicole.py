28864ee1b563da4191d8f087232e60794c8e0e6a
Nicole Arwood <nsarwood@gmail.com>
2023-02-11 15:36:45 -0500

    we got majors. we got minors.

--- nicole.py
+++ nicole.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 url = "https://catalog.unc.edu/undergraduate/programs-study/"
 data = requests.get(url)
@@ -9,10 +10,18 @@ soup = BeautifulSoup(html, 'html.parser')
 def main():
     majors = []
     minors = []
-    links = soup.find_all("li")
-    for link in links:
-        print(link.get('a href'))
-        print("\n")
+    minorTags = soup.find_all("a", string=re.compile("Minor"))
+    majorTags = soup.find_all("a", string=re.compile("Major"))
+    for item in minorTags:
+        minors.append(item.get_text())
+    del minors[0]
+    for item in majorTags:
+        majors.append(item.get_text())
+    del majors[0]
+    
+    print(minors)
+    print(majors)
+    
 
 if __name__ == "__main__":
     main()
