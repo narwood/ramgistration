030bdd9dc209fd56d9d2c0058cfe7d59bafe2003
Nicole Arwood <nsarwood@gmail.com>
2023-02-11 16:27:56 -0500

    birth of clicky

--- nicole.py
+++ nicole.py
@@ -7,20 +7,34 @@ data = requests.get(url)
 html = data.text
 soup = BeautifulSoup(html, 'html.parser')
 
-def main():
-    majors = []
+def majors():
+   majors = [] 
+   majorTags = soup.find_all("a", string=re.compile("Major"))
+   for item in majorTags:
+        majors.append(item.get_text())
+   del majors[0]
+   return majors
+
+def minors():
     minors = []
     minorTags = soup.find_all("a", string=re.compile("Minor"))
-    majorTags = soup.find_all("a", string=re.compile("Major"))
     for item in minorTags:
         minors.append(item.get_text())
     del minors[0]
-    for item in majorTags:
-        majors.append(item.get_text())
-    del majors[0]
+    return minors
+
+def clicky(program):
+    clickOn = soup.find_all("a", string=program)[0]
+    url2 = clickOn.get("href")[1::]
+    source = url + url2 + "#requirementstext"
+    dataClicky = requests.get(source)
+    htmlClicky = dataClicky.text
+    clickySoup = BeautifulSoup(htmlClicky, 'html.parser')
+    return clickySoup
     
-    print(minors)
-    print(majors)
+
+def main():  
+    print(clicky("American Studies Minor").prettify())
     
 
 if __name__ == "__main__":
