ac751024263027e6fcae73ec4d974702361887f3
Nicole Arwood <nsarwood@gmail.com>
2023-02-11 17:09:57 -0500

    clicky diagnostic

--- nicole.py
+++ nicole.py
@@ -26,15 +26,31 @@ def minors():
 def clicky(program):
     clickOn = soup.find_all("a", string=program)[0]
     url2 = clickOn.get("href")[1::]
-    source = url + url2 + "#requirementstext"
+    source = "https://catalog.unc.edu/" + url2 + "#requirementstext"
     dataClicky = requests.get(source)
     htmlClicky = dataClicky.text
     clickySoup = BeautifulSoup(htmlClicky, 'html.parser')
-    return clickySoup
-    
+    tables = clickySoup.find_all("table", class_="sc_courselist")
+    return tables
+
+def clickyDiagnostic():
+    sadCount = 0
+    problemPrograms = []
+    happyCount = 0
+    programs = majors() + minors()
+    for program in programs:
+        if len(clicky(program)) == 0:
+            sadCount += 1
+            problemPrograms.append(program)
+        else:
+            happyCount += 1
+    print("total: " + str(len(programs)))
+    print("sad: " + str(sadCount))
+    print("happy: " + str(happyCount))
+    print(problemPrograms)
 
 def main():  
-    print(clicky("American Studies Minor").prettify())
+    
     
 
 if __name__ == "__main__":
