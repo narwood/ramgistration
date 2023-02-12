03b27843de119da194f097770ad2ae25b4ec8e34
Suzanna Moran <smoran@unc.edu>
2023-02-12 01:51:33 -0500

    updated shitty

--- suzanna.py
+++ suzanna.py
@@ -11,26 +11,32 @@ soup = BeautifulSoup(html, 'html.parser')
 def main():
     print("running")
     megaclass = []
+    program_classes = []
     megaprereq = []
-    megagens = []
+    classes = []
     links = soup.select('a[href^="/courses/"]')
     links = links[3:152]
 
     for link in links:
+        #accesses class catalogs for each program
         subUrl = link.attrs['href']
         subObj = BeautifulSoup(urlopen("https://catalog.unc.edu" + subUrl), 'html.parser')
         #program = link.get_text()[0: len(link.get_text()) - 7]
         #abbr = getAbbr(link.get_text())
         #link = (getLink(subObj, subUrl).get('href'))
-        classes = getClasses(subObj)
-       # prereqs = getPrereq(subObj)
-        megaclass.append(', '.join(classes))
-    print(megaclass[:10])    
-        # for course in megaclass:
-        #     prereqs = getPrereq(course)
-        #     geneds = getGens(course)
-        #     megaprereq.append(', '.join(prereqs))
-        #     megagens.append(', '.join(geneds))
+        program_classes = getClasses(subObj)
+        for course in program_classes:
+            classes.append(course)
+    yeah = getPrereq(BeautifulSoup(urlopen("https://catalog.unc.edu/courses/aero/"), features="html.parser"))
+    print(yeah)
+    #megaprereq.append(prereqs)
+    #print(classes[:15])
+    #print(prereqs) #MAKE .JOIN LIST OF PREREQ LISTS
+    #for course in megaclass:
+       # prereqs = getPrereq(course)
+       # geneds = getGens(course)
+       # megaprereq.append(', '.join(prereqs))
+       # megagens.append(', '.join(geneds))
 
     #     subObj = BeautifulSoup(urlopen("https://catalog.unc.edu" + subUrl), 'html.parser')
     #     link = getLink(subObj, subUrl)
@@ -38,9 +44,13 @@ def main():
     
 
 
-# def getLink(subObj, subUrl):
-#     link = subObj.find('a', href=re.compile("/courses/"))
-#     return link
+def getLink(subObj):
+    link_list = []
+    clickOn = subObj.find_all("div", {'class':'cols no indent'})
+
+    link = soup.select()
+    link_list.append(link)
+    return link_list
 
 # def getAbbr(program):
 #     abbr = program[len(program) - 5:len(program) -1]
@@ -51,16 +61,52 @@ def getClasses(subObj):
     classes = subObj.find_all('div',{'class':'cols noindent'})
     for course in classes:
         course = course.get_text()
-        course = course.replace("\xa0", "")
-        course = course.replace(".", ". ")
+        #course = course.replace("\xa0", "")
+        course = course[:8]
         if(course != "" and course != " "):
+            if (course[len(course) - 1] == "."):
+                course = course[:7]
             class_list.append(course)
 
-    return class_list
+    return (class_list)
 
 #def getPrereq(subObj):
 
+def getPrereq(subObj):
+    prereq_list_big = []
+    clickOn = subObj.find_all("div", {'class':'courseblock'})
+    for course in clickOn:
+        print(course)
+        final_prereqs = []
+        prereq_list = []
+        url2 = course.get("href")
+        print("URL: " + str(url2))
+        if(url2 != ""):
+            source = "https://catalog.unc.edu" + str(url2)
+            print("src: " + source)
+            dataClicky = requests.get(source)
+        #prereq page accessed
+            htmlClicky = dataClicky.text
+            clickySoup = BeautifulSoup(htmlClicky, 'html.parser')
+            prereq = getClasses(clickySoup)
+            print("pre: " + '. '.join(prereq))
+        else: 
+            prereq = "None"
+        prereq_list.append(prereq)
 
+    #     if (len(prereq_list) == 0):
+    #         print("none")
+    #         prereq_list_big.append("N/A")
+        
+    #     else:
+    #         print("adding prereqs to list")
+    #         for prereq in prereq_list:
+    #             prereq = prereq.get_text()
+    #             prereq = prereq.replace("\xa0", "")
+    #             prereq = prereq.replace(".", ". ")
+    #             final_prereqs.append(prereq)
+    #         prereq_list_big.append(prereq)
+    return prereq_list_big
 
 if __name__ == "__main__":
     main()
